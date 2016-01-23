ffb4177400ade412ee535028214b0fa330b47cbd
Miguel Angel Quintana <quintana.mige@gmail.com>
2016-01-23 01:13:23 -0800

    Adds User Assosiation to Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,16 @@
 include RandomData
+
+# Create Users
+5.times do
+  User.create!(
+# #3
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 #Create topics
 15.times do
   Topic.create!(
@@ -12,6 +24,7 @@ topics = Topic.all
 50.times do
 
   Post.create!(
+  user:   users.sample,
   topic: topics.sample,
   title:  RandomData.random_sentence,
   body:   RandomData.random_paragraph
@@ -27,8 +40,16 @@ posts = Post.all
   body: RandomData.random_paragraph
  )
 end
+#My User test
+user = User.first
+user.update_attributes!(
+  email: 'quintana.mige@gmail.com',
+  password: 'helloworld'
+)
+
 
 puts "Seed Finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
