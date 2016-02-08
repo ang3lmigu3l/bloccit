a8b1bddec5d7eab447b7d613dd3f8d9bf793933d
Miguel Angel Quintana <quintana.mige@gmail.com>
2016-02-07 23:56:30 -0800

    Unfinished Checkpoint

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160131064931) do
+ActiveRecord::Schema.define(version: 20160208060419) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -121,6 +121,7 @@ ActiveRecord::Schema.define(version: 20160131064931) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
