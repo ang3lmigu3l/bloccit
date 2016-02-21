0fc48d4a9bb6058b3cc2e2c300d92fcda390ded7
Miguel Angel Quintana <quintana.mige@gmail.com>
2016-02-20 23:06:18 -0800

    Adds USER TOPIC API

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,8 @@ get 'about' => 'welcome#about'
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
-      resources :topics, only: [:index, :show]
+      resources :topics, except: [:edit, :new]
+      
     end
   end
 end
